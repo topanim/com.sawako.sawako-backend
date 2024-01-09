2588dd2fef9a7cebe1a7c69eafb61c4a8df9adbd
topanim <gorogannisan641@gmail.com>
2024-01-10 02:07:44 +0300

    updated: testpattern.json

--- src/main/kotlin/com/sawako/database/guilds/settings/GuildSettingsController.kt
+++ src/main/kotlin/com/sawako/database/guilds/settings/GuildSettingsController.kt
@@ -4,9 +4,14 @@ import java.io.File
 
 
 object GuildSettingsController {
-    private const val SETTINGS_PATTERN_PATH= "/settings/testpattern.json"
+    private const val SETTINGS_PATTERN_PATH = "/settings/testpattern.json"
 
-    fun getDefaultSettings(): String = object {}.javaClass.getResource(SETTINGS_PATTERN_PATH)?.readText() ?: "{}"
+    fun getDefaultSettings(): String {
+        (object {}.javaClass.getResource(SETTINGS_PATTERN_PATH)?.readText() ?: "{}").let {
+            println(it)
+            return it
+        }
+    }
 
     fun setNewDefault(settings: String) {
         val resourceUrl = object {}.javaClass.getResource(SETTINGS_PATTERN_PATH)
--- src/test/kotlin/com/sawako/ApplicationTest.kt
+++ src/test/kotlin/com/sawako/ApplicationTest.kt
@@ -1,11 +1,12 @@
 package com.sawako
 
-import com.sawako.plugins.*
+import com.sawako.plugins.configureRouting
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
